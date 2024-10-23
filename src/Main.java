4101e623ab274abda18d7fc1e9a72bb0dc8af891
rizqullahnusa22 <rizqullah.nusa22@gmail.com>
2024-10-23 16:17:52 +0700

    ayy

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.printf("Hello sayangg");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
